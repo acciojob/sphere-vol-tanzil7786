a64ba1feb707fc6bf96c7906b34952fbdab35ac8
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 13:38:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function volume_sphere() {
+function volume_sphere(def) {
     //Write your code here
+	def.preventDefault();
 	let radius= parseFloat(document.getElementById("radius").value);
-	let volume= (4 / 3) * pi * Math.pow(radius, 3);
-	document.getElementById("volume").value=volume.toFixed(2);
+	let volume= (4 / 3) * Math.PI * Math.pow(radius, 3);
+	let roundedValue=volume.toFixed(4);
+	window.document.getElementById("volume").value=roundedValue;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
